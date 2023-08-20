c7b41994a902cec4484cc4ca40251c89c37a7226
abhay <abhaytyagi2513@gmail.com>
2023-08-20 23:08:28 +1000

    added directives

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,7 +2,7 @@
   <header class="w-full text-sm">
     <div class="fixed top-0 left-0 h-16 w-full bg-white">
       <div class="mx-auto flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8">
-        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
+        <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
       </div>
     </div>
   </header>
@@ -13,7 +13,8 @@ export default {
   name: 'MainNav',
   data() {
     return {
-      company: 'Umbrella Corp'
+      company: 'Umbrella Corp',
+      url: 'https://careers.google.com'
     }
   }
 }
