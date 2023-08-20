0018e764b14920fb646dc80124fcefdefa76e00e
abhay <abhaytyagi2513@gmail.com>
2023-08-20 19:54:06 +1000

    prog

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,7 +2,8 @@
   <header class="w-full text-sm">
     <div class="fixed top-0 left-0 h-16 w-full bg-white">
       <div class="mx-auto flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8">
-        <a href="/" class="flex h-full items-center text-xl">Website Name</a>
+        <a href="/" class="flex h-full items-center text-xl">{{ company }}</a>
+        <h2 class="ml-8 flex h-full items-center">Developed by {{ author }}</h2>
       </div>
     </div>
   </header>
@@ -10,6 +11,12 @@
 
 <script>
 export default {
-  name: 'MainNav'
+  name: 'MainNav',
+  data() {
+    return {
+      company: 'Umbrella Corp',
+      author: 'perriDplatypus'
+    }
+  }
 }
 </script>
