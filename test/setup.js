6e372165e3277243ecb27d998a47140e63b89da8
abhay <abhaytyagi2513@gmail.com>
2023-08-20 22:40:22 +1000

    component unit tests

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,5 @@
 import { cleanup } from '@testing-library/vue'
-import { matchers } from '@testing-library/jest-dom/matchers'
+import matchers from '@testing-library/jest-dom/matchers'
 import { expect, afterEach } from 'vitest'
 
 expect.extend(matchers)
---
+++ test/unit/components/MainNav.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/vue'
+import MainNav from '@/components/MainNav.vue'
+
+describe('MainNav', () => {
+  it('Displays company name', () => {
+    render(MainNav)
+    screen.debug()
+  })
+})
