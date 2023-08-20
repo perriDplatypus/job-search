b51302828b97221a210b34bb7917c40f5302086f
abhay <abhaytyagi2513@gmail.com>
2023-08-20 17:34:26 +1000

    prog

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div class="wrapper">
     <HelloWorld msg="You did it!" />
+    <HelloWorld msg="Fuck You" />
   </div>
 </template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
