93d73d8c3dbd70c1c8a0ad0aba1d0bb2af625acb
abhay <abhaytyagi2513@gmail.com>
2023-08-21 00:06:49 +1000

    added tests for navBar

--- test/unit/components/MainNav.test.js
+++ test/unit/components/MainNav.test.js
@@ -1,10 +1,25 @@
 import { render, screen } from '@testing-library/vue'
 import MainNav from '@/components/MainNav.vue'
+import { expect } from 'vitest'
 
 describe('MainNav', () => {
   it('Displays company name', () => {
     render(MainNav)
     const companyName = screen.getByText('Umbrella Corp')
     expect(companyName).toBeInTheDocument()
+  })
+
+  it('Displays menu items for navigation', () => {
+    render(MainNav)
+    const navigationMenuItems = screen.getAllByRole('listitem')
+    const navigationMenuTexts = navigationMenuItems.map((item) => item.textContent)
+    expect(navigationMenuTexts).toEqual([
+      'Teams',
+      'Locations',
+      'Life at Umbrella',
+      'How we hire',
+      'Students',
+      'Jobs'
+    ])
   })
 })
