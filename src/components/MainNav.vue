89efda431e89fba0364655314b4f402194bc9e4a
abhay <abhaytyagi2513@gmail.com>
2023-08-20 23:33:10 +1000

    made menu items

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -3,6 +3,13 @@
     <div class="fixed top-0 left-0 h-16 w-full bg-white">
       <div class="mx-auto flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8">
         <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
+        <nav class="ml-3 h-full">
+          <ul class="flex h-full list-none">
+            <li v-for="menuItem in menuItems" :key="menuItem" class="h-full">
+              <a href="" class="ml-9 flex h-full items-center py-2.5">{{ menuItem }}</a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </div>
   </header>
@@ -14,7 +21,8 @@ export default {
   data() {
     return {
       company: 'Umbrella Corp',
-      url: 'https://careers.google.com'
+      url: 'https://careers.google.com',
+      menuItems: ['Teams', 'Locations', 'Life at Umbrella', 'How we hire', 'Students', 'Jobs']
     }
   }
 }
