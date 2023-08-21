8ac7635b18b1cf72e761d65b91f623adbe6bfce3
abhay <abhaytyagi2513@gmail.com>
2023-08-21 20:11:39 +1000

    created Button and ProfileImage components

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,7 +2,7 @@
   <header class="w-full text-sm">
     <div class="fixed top-0 left-0 h-16 w-full bg-white">
       <div class="mx-auto flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8">
-        <a :href="url" class="flex h-full items-center text-xl">{{ company }}</a>
+        <a href="" class="flex h-full items-center text-xl">{{ company }}</a>
         <nav class="ml-12 h-full">
           <ul class="flex h-full list-none">
             <li v-for="menuItem in menuItems" :key="menuItem" class="h-full ml-9 first:ml-0">
@@ -10,14 +10,22 @@
             </li>
           </ul>
         </nav>
+        <div class="ml-auto flex h-full items-center">
+          <ActionButton />
+        </div>
       </div>
     </div>
   </header>
 </template>
 
 <script>
+import ActionButton from '@/components/ActionButton.vue'
+
 export default {
   name: 'MainNav',
+  components: {
+    ActionButton
+  },
   data() {
     return {
       company: 'Umbrella Corp',
---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,14 @@
+<template>
+  <img :src="imageLink" class="h-8 w-8 rounded-3xl object-contain" />
+</template>
+
+<script>
+export default {
+  name: 'ProfileImage',
+  data() {
+    return {
+      imageLink: 'https://gfycat.com/adventurousleafygoldeneye'
+    }
+  }
+}
+</script>
