a25fd437cc429ffbf581b6c656576a70f11b4fb5
abhay <abhaytyagi2513@gmail.com>
2023-08-21 22:11:26 +1000

    prog

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,13 +1,30 @@
 <template>
-  <button
-    class="rounded border-0 px-5 py-3 font-medium text-white bg-brand-blue-1 hover:shadow-blue"
-  >
-    Sign in
+  <button :class="{ primary: primary }">
+    {{ text }}
   </button>
 </template>
 
 <script>
 export default {
-  name: 'ActionButton'
+  name: 'ActionButton',
+  props: ['text'],
+  data() {
+    return {
+      primary: true
+    }
+  }
 }
 </script>
+
+<style scoped>
+button {
+  @apply rounded border-0 px-5 py-3 font-medium;
+}
+
+.primary {
+  @apply border-0 bg-brand-blue-1 text-white hover:shadow-blue;
+}
+
+.secondary {
+}
+</style>
