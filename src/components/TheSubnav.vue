c0a2054156b53cd4035942a6f20c78df280153aa
abhay <abhaytyagi2513@gmail.com>
2023-08-22 20:00:00 +1000

    added data

--- src/components/TheSubnav.vue
+++ src/components/TheSubnav.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full h-16 border-b border-solid border-brand-gray-1 bg-white">
     <div class="flex items-center h-full px-8">
-      <div>
+      <div v-if="onJobResultPage">
         <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
         <span><span class="text-brand-green-1">1653</span> jobs matched</span>
       </div>
@@ -11,6 +11,11 @@
 
 <script>
 export default {
-  name: 'TheSubnav'
+  name: 'TheSubnav',
+  data() {
+    return {
+      onJobResultPage: true
+    }
+  }
 }
 </script>
---
+++ test/unit/components/TheSubnav.test.js
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/vue'
+import { describe, expect } from 'vitest'
+
+import TheSubnav from '@/components/TheSubNav.vue'
+
+describe('TheSubnav', () => {
+  describe('when the user is on jobs page', () => {
+    it('displays job count', () => {
+      render(TheSubnav, {
+        data() {
+          return {
+            onJobResultPage: true
+          }
+        }
+      })
+
+      const jobCount = screen.getByText('1653')
+      expect(jobCount).toBeInTheDocument()
+    })
+  })
+
+  describe('when the user is not on jobs page', () => {
+    it('does not display job count', () => {
+      render(TheSubnav, {
+        data() {
+          return {
+            onJobResultPage: false
+          }
+        }
+      })
+
+      const jobCount = screen.queryByText('1653')
+      expect(jobCount).not.toBeInTheDocument()
+    })
+  })
+})
