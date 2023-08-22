949ac3791802f2e46ab67536bbcddddf7913664b
abhay <abhaytyagi2513@gmail.com>
2023-08-22 20:40:44 +1000

    added main page content init

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="w-full text-sm">
+  <header :class="['w-full', 'text-sm', headerHeightClass]">
     <div class="fixed top-0 left-0 h-16 w-full bg-white">
       <div class="mx-auto flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8">
         <a href="" class="flex h-full items-center text-xl">{{ company }}</a>
@@ -12,7 +12,7 @@
         </nav>
         <div class="ml-auto flex h-full items-center">
           <a :href="url">
-            <font-awesome-icon :icon="['fas', 'up-right-from-square']" class="mr-7" />
+            <font-awesome-icon :icon="['fas', 'up-right-from-square']" class="mr-6" />
           </a>
           <ProfileImage v-if="isLoggedIn" />
           <ActionButton v-else text="Sign in" type="primary" @click="loginUser" />
@@ -41,6 +41,14 @@ export default {
       url: 'https://careers.google.com',
       menuItems: ['Teams', 'Locations', 'Life at Umbrella', 'How we hire', 'Students', 'Jobs'],
       isLoggedIn: false
+    }
+  },
+  computed: {
+    headerHeightClass() {
+      return {
+        'h-16': !this.isLoggedIn,
+        'h-32': this.isLoggedIn
+      }
     }
   },
   methods: {
---
+++ src/components/TheHero.vue
@@ -0,0 +1,9 @@
+<template>
+  <section>I can be your hero baby</section>
+</template>
+
+<script>
+export default {
+  name: 'TheHero'
+}
+</script>
