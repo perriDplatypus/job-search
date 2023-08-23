21083d72ef1c585ba1d3a0e66c10e338d94cc075
abhay <abhaytyagi2513@gmail.com>
2023-08-23 22:58:50 +1000

    added logo

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -3,7 +3,12 @@
     <div class="grid grid-cols-12">
       <div class="col-start-1 col-span-1"></div>
       <div class="col-start-2 col-span-5"><the-headline /></div>
-      <div class="col-start-7 col-span-5"></div>
+      <div class="col-start-7 col-span-5 self-center justify-self-center">
+        <img
+          class="h-80 w-80 object-contain"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0e/Umbrella_Corporation_logo.svg"
+        />
+      </div>
       <div class="col-start-12 col-span-1"></div>
     </div>
   </section>
