f273ba85252540a7416676aaede75deb020b9e6c
abhay <abhaytyagi2513@gmail.com>
2023-08-23 22:13:25 +1000

    cleanup

--- src/components/TheHero.vue
+++ src/components/TheHero.vue
@@ -1,10 +1,10 @@
 <template>
-  <section class="flex flex-col h-screen bg-yellow-200 pt-10 pb-20">
+  <section class="flex flex-col h-screen pt-10 pb-20">
     <div class="grid grid-cols-12">
-      <div class="col-start-1 col-span-1 border border-blue-700">1 xolumn</div>
-      <div class="col-start-2 col-span-5 border border-blue-700"><the-headline /></div>
-      <div class="col-start-7 col-span-5 border border-blue-700">1 xolumn</div>
-      <div class="col-start-12 col-span-1 border border-blue-700">1 xolumn</div>
+      <div class="col-start-1 col-span-1"></div>
+      <div class="col-start-2 col-span-5"><the-headline /></div>
+      <div class="col-start-7 col-span-5"></div>
+      <div class="col-start-12 col-span-1"></div>
     </div>
   </section>
 </template>
